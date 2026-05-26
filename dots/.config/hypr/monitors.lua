426f0bfdb199fa98303d2e88d0b26e4a366ed88e
Hai Ha <haiminh.utc@gmail.com>
2026-05-26 14:49:27 +0700

    add monitors and workspaces

---
+++ dots/.config/hypr/monitors.lua
@@ -0,0 +1,20 @@
+hl.monitor({
+	output = "desc:ASUSTek COMPUTER INC VG27AQ1A S9LMQS053683",
+	mode = "2560x1440@144.01",
+	position = "0x0",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "eDP-1",
+	mode = "1920x1200@59.95",
+	position = "2560x0",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "desc:Audio Processing Technology  Ltd HDMI",
+	mode = "1920x1080@60.00",
+	position = "0x1440",
+	scale = 1,
+})
