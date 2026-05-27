20d432c8dcd21f523ae300ec49e346e9ed8cff9e
Hai Ha <haiminh.utc@gmail.com>
2026-05-27 11:01:21 +0700

    update hypridle and rules

--- dots/.config/hypr/custom/rules.lua
+++ dots/.config/hypr/custom/rules.lua
@@ -1,1 +1,4 @@
-
+hl.layer_rule({ match = { namespace = "quickshell:workspaceBlurOverlay" }, order = -1 })
+hl.layer_rule({ match = { namespace = "quickshell:workspaceBlurOverlay" }, blur = true })
+hl.layer_rule({ match = { namespace = "quickshell:workspaceBlurOverlay" }, ignore_alpha = 0.0 })
+hl.layer_rule({ match = { namespace = "quickshell:workspaceBlurOverlay" }, animation = "fade" })
