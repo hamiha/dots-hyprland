e5b5b5148ebbfeca4ac7d6b4372d09efc0ab420a
Hai Ha <haiminh.utc@gmail.com>
2026-06-09 11:02:09 +0700

    skip microTex

--- sdata/dist-arch/install-deps.sh
+++ sdata/dist-arch/install-deps.sh
@@ -92,7 +92,7 @@ install-local-pkgbuild() {
 # Install core dependencies from the meta-packages
 metapkgs=(./sdata/dist-arch/illogical-impulse-{audio,backlight,basic,fonts-themes,kde,portal,python,screencapture,toolkit,widgets})
 metapkgs+=(./sdata/dist-arch/illogical-impulse-hyprland)
-metapkgs+=(./sdata/dist-arch/illogical-impulse-microtex-git)
+# metapkgs+=(./sdata/dist-arch/illogical-impulse-microtex-git) # Skipped: MicroTeX LaTeX rendering not used at runtime
 metapkgs+=(./sdata/dist-arch/illogical-impulse-quickshell-git)
 metapkgs+=(./sdata/dist-arch/illogical-impulse-bibata-modern-classic-bin)
 
